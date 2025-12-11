aa278222da74897ff1d5b33279e2004a62fd0527
yanseeeeeeeeee <anasemenova56966@gmail.com>
2025-12-11 23:48:02 +0700

    добавлены фрагменты

---
+++ course_work-master/app/src/main/java/com/example/coursework/ui/theme/Main.java
@@ -0,0 +1,18 @@
+package com.example.coursework.ui.theme;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coursework.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.main);
+    }
+}
